ccd59830d343eada9e76bb37ee4217b908361a48
Josh Gwaltney <xproj2501x@gmail.com>
2018-03-21 08:13:56 -0700

    A collection of enums for data tables

---
+++ src/server/enums/nature.js
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////
+// Definitions
+////////////////////////////////////////////////////////////////////////////////
+/**
+ * Enum for natures
+ * @readonly
+ * @enum {int}
+ */
+const NATURE = {
+  HARDY: 0x00,
+  LONELY: 0x01,
+  BRAVE: 0x02,
+  ADAMANT: 0x03,
+  NAUGHTY: 0x04,
+  BOLD: 0x05,
+  DOCILE: 0x06,
+  RELAXED: 0x07,
+  IMPISH: 0x08,
+  LAX: 0x09,
+  TIMID: 0x0A,
+  HASTY: 0x0B,
+  SERIOUS: 0x0C,
+  JOLLY:0x0D,
+  NAIVE: 0x0E,
+  MODEST: 0x0F,
+  MILD: 0x10,
+  QUIET: 0x11,
+  BASHFUL: 0x12,
+  RASH: 0x13,
+  CALM: 0x14,
+  GENTLE: 0x15,
+  SASSY: 0x16,
+  CAREFUL: 0x17,
+  QUIRKY: 0x18
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Exports
+////////////////////////////////////////////////////////////////////////////////
+export default NATURE;
