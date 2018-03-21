c89c732b6d056b6302ba3e358710b0c175a571a6
Josh Gwaltney <xproj2501x@gmail.com>
2018-03-21 12:50:28 -0700

    Data tables

---
+++ src/server/enums/index.js
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+// Imports
+////////////////////////////////////////////////////////////////////////////////
+import ABILITY from './ability';
+import BODY_COLOR from './body-color';
+import EGG_GROUP from './egg-group';
+import EVOLUTION_METHOD from './evolution-method';
+import GROWTH from './growth';
+import ITEM from './item';
+import MOVE from './move';
+import MOVE_CATEGORY from './move-category';
+import NATURE from './nature';
+import POCKET from './pocket';
+import SPECIES from './species';
+import TYPE from './type';
+
+////////////////////////////////////////////////////////////////////////////////
+// Exports
+////////////////////////////////////////////////////////////////////////////////
+export default {ABILITY, BODY_COLOR, EGG_GROUP, EVOLUTION_METHOD, GROWTH, ITEM, MOVE, MOVE_CATEGORY, NATURE, POCKET,
+  SPECIES, TYPE};
